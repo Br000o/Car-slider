ac3c2e1155f8ae40723bee881fc41dd05e9e0f29
Br000o <ah.vibol008@gmail.com>
2025-08-09 23:15:17 +0700

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -48,14 +48,11 @@ const swiperHome = new Swiper(".home__swiper", {
 });
 
 /*=============== GSAP ANIMATION ===============*/
-
-/*=============== ADD BLUR HEADER ===============*/
-
-/*=============== REMOVE MENU MOBILE ===============*/
-
-/*=============== SWIPER CAR ===============*/
-
-/*=============== GSAP ANIMATION ===============*/
+gsap.from('.home__panel-1', {y: -1000, duration: 2})
+gsap.from('.home__panel-2', {y: 1000, duration: 2})
+gsap.from('.home__image', {x: 1000, duration: 2})
+gsap.from('.home__titles', {y: 100, opacity: 0, delay: 2})
+gsap.from('.home__title', {y: 100, opacity: 0, delay: 2.1})
 
 /*=============== ADD BLUR HEADER ===============*/
 const blurHeader = () =>{
